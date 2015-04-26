2d650062e8e3c44480246cbe54e2b8137172b190
Randall Rushing <randall.rushing@gmail.com>
2015-04-25 20:08:58 -0700

    Manta: bring up for Eos

---
+++ eos.mk
@@ -0,0 +1,27 @@
+# Resolution values for bootanimation
+TARGET_SCREEN_HEIGHT := 1600
+TARGET_SCREEN_WIDTH := 2560
+
+# Release name
+PRODUCT_RELEASE_NAME := Nexus10
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/eos/config/common_full_tablet_wifionly.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/eos/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/manta/full_manta.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := manta
+PRODUCT_NAME := eos_manta
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 10
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=mantaray \
+    BUILD_FINGERPRINT=google/mantaray/manta:5.0.2/LRX22G/1649326:user/release-keys \
+    PRIVATE_BUILD_DESC="mantaray-user 5.0.2 LRX22G 1649326 release-keys"
